9f976eec29a8386fdaa4ce26ab888e39dee9816e
Elyrah408 <e.rahimi2019@gmail.com>
2024-11-07 09:50:10 +0100

    Add files via upload

---
+++ 7designb/final new/js/transalation.js
@@ -0,0 +1,9 @@
+const transalation = {  fr: {select:"Langue", colorpic:"Sélectionnez votre couleur", submit:"soumettre"},
+                        en: {select:"Language", colorpic:"Select your color", submit:"Submit"},
+                        ar: {select:"لغة", colorpic:"حدد اللون الخاص بك", submit:"يُقدِّم"}
+                      };
+
+    localStorage.setItem("transalation",transalation);
+
+
+
