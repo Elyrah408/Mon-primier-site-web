8249bb81a0a1fa5f85d29ec796dfc07d150ef897
Elyrah408 <e.rahimi2019@gmail.com>
2024-11-18 09:23:15 +0100

    Delete 7designb/final new directory

--- 7designb/final new/js/transalation.js
+++
@@ -1,9 +0,0 @@
-const transalation = {  fr: {select:"Langue", colorpic:"Sélectionnez votre couleur", submit:"soumettre"},
-                        en: {select:"Language", colorpic:"Select your color", submit:"Submit"},
-                        ar: {select:"لغة", colorpic:"حدد اللون الخاص بك", submit:"يُقدِّم"}
-                      };
-
-    localStorage.setItem("transalation",transalation);
-
-
-
